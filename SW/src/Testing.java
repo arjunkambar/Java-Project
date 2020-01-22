1388efa1eb1cd2b7cc8531ccc4a34945cdcadbdc
Arjun <mallikarjunkambar661@gmail.com>
2020-01-22 21:19:14 +0530

    [DTEst

--- SW/src/Testing.java
+++ SW/src/Testing.java
@@ -2,7 +2,13 @@
 public class Testing {
 
 	public static void main(String[] args) {
-		System.out.println("Hello123");
+		for(int i=0;i<=5;i++){
+			for(int j=0;j<=i;j++){
+				System.out.print("*");
+			}
+			System.out.println("");
+		}
+
 	}
 
 }
