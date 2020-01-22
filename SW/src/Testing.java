63bda6194a080f26da51861bbaabe4eed8204113
arjunkambar <59144692+arjunkambar@users.noreply.github.com>
2020-01-22 21:50:46 +0530

    Update Testing.java

--- SW/src/Testing.java
+++ SW/src/Testing.java
@@ -2,17 +2,11 @@
 public class Testing {
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
+
 		for(int i=0;i<=5;i++){
 			for(int j=5;j>=i;j--){
 				System.out.print("*");
 			}
 			System.out.println("");
-		}
+		}}}
 
-=======
-		System.out.println("Hello123");
->>>>>>> parent of 1388efa... [DTEst
-	}
-
-}
