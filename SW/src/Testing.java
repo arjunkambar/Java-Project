575d5278619bf50cd20b9e4befc41fb289fc851a
arjunkambar <59144692+arjunkambar@users.noreply.github.com>
2020-01-24 00:06:56 +0530

    Update Testing.java

--- SW/src/Testing.java
+++ SW/src/Testing.java
@@ -7,11 +7,7 @@ public class Testing {
 			}
 			System.out.println();
 		}
-	        for(int i=0;i<=5;i++){
-			for(int j=0;j<=i;j++){
-				System.out.print("*");
-			}
-			System.out.println();}}}
+	        }}
 
 
 
