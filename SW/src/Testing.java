b30274042243421b864823657cafcaf20f96e441
Arjun <mallikarjunkambar661@gmail.com>
2020-01-23 23:06:23 +0530

    Descending in NEW

--- SW/src/Testing.java
+++ SW/src/Testing.java
@@ -3,7 +3,7 @@ public class Testing {
 
 	public static void main(String[] args) {
 	for(int i=0;i<=5;i++){
-			for(int j=0;j<=i;j++){
+			for(int j=5;j>=i;j--){
 				System.out.print("*");
 			}
 			System.out.println("");
