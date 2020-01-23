ba11aacbf2813421a42b6ab6a6e54c68fe8f49cf
Arjun <mallikarjunkambar661@gmail.com>
2020-01-23 23:23:53 +0530

    Two patterns

--- SW/src/Testing.java
+++ SW/src/Testing.java
@@ -6,7 +6,12 @@ public class Testing {
 			for(int j=5;j>=i;j--){
 				System.out.print("*");
 			}
-			System.out.println("");
+			System.out.println("");}
+			for(int i=1;i<=5;i++){
+				for(int j=0;j<=i;j++){
+					System.out.print("*");
+				}
+				System.out.println("");
 		}}}
 
 
