eef3c08f531973b0cbf12434afd7294757820101
Arjun <mallikarjunkambar661@gmail.com>
2020-01-25 13:27:41 +0530

    Second commit in NEW

--- SW/src/Testing.java
+++ SW/src/Testing.java
@@ -7,11 +7,12 @@ public class Testing {
 			}
 			System.out.println();
 		}
-	        for(int i=0;i<=5;i++){
-			for(int j=0;j<=i;j++){
-				System.out.print("*");
-			}
-			System.out.println();}}}
+	for(int i=0;i<=5;i++){
+		for(int j=5;j>=i;j--){
+			System.out.print("*");
+		}
+		System.out.println();
+	}}}
 
 
 
